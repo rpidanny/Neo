6f89494f98641c691df5568731063e64d23a61af
rpidanny <abhishekmaharjan1993@gmail.com>
2018-06-11 20:49:44 +0200

    added signs example

---
+++ examples/Signs/Signs.ino
@@ -0,0 +1,25 @@
+#include <Neo.h>
+#include <Neo_signs.h>
+
+#define SPEED 100
+
+Neo disp(0, 7, 0, 4);
+
+byte frame[8];
+const uint8_t SIGNS_COUNT = sizeof(SIGN)/8;
+
+void setup() {
+  delay(100);
+  disp.init();
+  disp.setBrightness(0);
+  disp.clearDisplay();
+}
+
+void loop() {
+  for (int i = 0; i < 57 ; i++)
+  {
+    memcpy_P(frame, SIGN + 8 * i, 8);
+    disp.renderDisplay(0, frame);
+    delay(SPEED);
+  }
+}
