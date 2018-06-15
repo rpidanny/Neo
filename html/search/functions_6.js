b9c76892256988f9670c44204922ae91715713c8
Travis CI <travis@travis-ci.org>
2018-06-15 22:58:29 +0000

    Deploy code docs to GitHub Pages Travis build: 22
    Commit: 89d5aae8da96ae9e689faa1c51c06626178bc49b

--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['render',['render',['../class_neo.html#adebcea23187c03c878f8ed416b415753',1,'Neo']]],
-  ['renderdisplay',['renderDisplay',['../class_neo.html#ab9f2285b80acc813b9cef9d5ecb1bb85',1,'Neo']]]
+  ['renderdisplay',['renderDisplay',['../class_neo.html#ab9f2285b80acc813b9cef9d5ecb1bb85',1,'Neo']]],
+  ['renderrow',['renderRow',['../class_neo.html#a112510a00e27a5379b271f4faf083ccd',1,'Neo']]]
 ];
